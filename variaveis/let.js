6ba5202e94a309d42271864850b5470733a85927
alannasant0s <alannacom2n@gmail.com>
2025-07-09 21:00:52 -0300

    inserindo arquivos

---
+++ variaveis/let.js
@@ -0,0 +1,14 @@
+// variaveis
+let username = "João"; // string
+// let permite declarar variáveis que podem ser alteradas posteriormente
+let age = 30; // number
+let adress = "Rua doutor agra 101 - São José"; // string
+let isStudent = true; // boolean
+
+// Exemplo de uso de variaveis null, undefined e vazia
+
+let emptyVariable = null; // variavel vazia, mas definida como null
+let undefinedVariable; // variavel indefinida  undefined 
+let zero = 0; // number, mas é considerado "falsy" em JavaScript
+
+console.log("O nome é " + username + " e a idade é " + age + " anos.");
---
+++ variaveis/object.js
@@ -0,0 +1,9 @@
+// chave-valor
+const person = {
+  name: "João",
+  age: 30,
+  address: "Rua doutor agra 101 - São José",
+  isStudent: true
+};
+
+const colors = ["red", "green", "blue"]; // array
